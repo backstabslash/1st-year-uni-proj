a40fd2c4551741176177ca28f35588e5695a0d14
Rainbinder <71657988+Rainbinder@users.noreply.github.com>
2021-06-22 14:26:27 +0300

    Add files via upload

--- Student.h
+++ Student.h
@@ -11,12 +11,12 @@ class Student : public Human
 private:
 	std::string grade;
 	std::vector<Mark> marks;
-	bool already_has_final(const std::string& subject);
 public:
 	Student();
 	Student(std::string full_name, int age, int height, std::string grade);
 	int count_final_mark(const std::string& subject);
 	void print_final_mark(const std::string& subject);
+	bool already_has_final(const std::string& subject);
 	void print_marks();
 	std::string short_info();
 	std::string GetGrade();
